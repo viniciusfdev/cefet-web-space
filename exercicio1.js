58541c986b2a4cd00e55c680d83f17d6f9f3c192
Vinícius França <viniciusfdev@gmai.com>
2021-06-20 11:53:20 -0300

    exercicio 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,23 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const G = 6.67e-11;
+
+function gravitLaw(m1, m2, d) {
+  if (d === 0) throw "Two bodies cannot occupy the same space";
+  return (G * m1 * m2) / Math.pow(d, 2);
+}
+
+const calcular = document.getElementById("calcular");
+const resultado = document.getElementById("resultado");
+
+calcular.onclick = function (event) {
+  const m1 = document.getElementById("massa1").value;
+  const m2 = document.getElementById("massa1").value;
+  const d = document.getElementById("distancia").value;
+
+  try {
+    resultado.value = gravitLaw(Number(m1), Number(m2), Number(d));
+  } catch (error) {
+    alert(error.message || error);
+  }
+};
